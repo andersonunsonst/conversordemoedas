e833f8a37695f6841a232b9143f6db2c59b120df
Maycon-Natan <mayconnatan7@gmail.com>
2023-11-30 17:09:46 -0300

    first commit

---
+++ lib/src/stores/moeda_store.dart
@@ -0,0 +1,40 @@
+import 'package:consumir_api/src/models/moeda_model.dart';
+import 'package:consumir_api/src/repositories/moeda_repository.dart';
+import 'package:consumir_api/src/states/moeda_state.dart';
+import 'package:flutter/material.dart';
+
+class MoedaStore extends ValueNotifier<MoedaState> {
+  final repository = MoedaRepository();
+
+  MoedaStore() : super(MoedaState.init());
+
+  Future<void> getMoedas() async {
+    final moedas = await repository.getMoedas();
+
+    value = value.copyWith(moedas: moedas);
+  }
+
+  Future<void> converter(String valorRaw) async {
+    final cotacao = await repository.cotacao(value.moedaIn, value.moedaOut);
+
+    final valor = double.parse(valorRaw);
+    final resultado = valor * cotacao;
+
+    value = value.copyWith(result: resultado.toStringAsFixed(2));
+  }
+
+  Future<void> selecionarMoedaIn(MoedaModel model) async {
+    value = value.copyWith(moedaIn: model);
+  }
+
+  Future<void> selecionarMoedaOut(MoedaModel model) async {
+    value = value.copyWith(moedaOut: model);
+  }
+
+  void switchMoedas() {
+    value = value.copyWith(
+      moedaIn: value.moedaOut,
+      moedaOut: value.moedaIn,
+    );
+  }
+}
